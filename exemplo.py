c8d5f952451518be248a73a88058970e352d3d55
Rei Cardoso <131529446+CardozeraAlfa@users.noreply.github.com>
2023-04-22 11:58:51 -0300

    Add files via upload

---
+++ exemplo.py
@@ -0,0 +1,18 @@
+print('Hello World')
+
+# comentário
+nome = "Lucas"
+_nome = "Lucas"
+Nome = input("Digite seu nome: ")
+
+if Nome == "Lucas":
+    print("Olá, Lucas")
+elif Nome == "Leo":
+    print ("Olá, Leo")
+
+    if True:
+        print("Olá, Leo")
+else:
+    print("Olá, desconhecido")
+
+    
